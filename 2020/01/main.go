d4054a1311af925d004811f88e0c57e745b8b347
Gabriel Marko <marko.gabriel.cz@gmail.com>
2024-11-02 17:23:07 +0100

    2020/01 - Solutions.

---
+++ 2020/01/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"github.com/suic86/aoc-go/util"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input := util.ReadFile("input.data")
+	fmt.Println("Solution 01: %i", part1(input))
+	fmt.Println("Solution 02: %i", part2(input))
+}
+
+func part1(input string) int {
+	seen := map[int]bool{}
+	ns := parseInput(input)
+	for _, n := range ns {
+		if seen[n] {
+			return n * (2020 - n)
+		}
+		seen[2020-n] = true
+	}
+	return -1 // unreachable
+}
+
+func part2(input string) int {
+	ns := parseInput(input)
+	ln := len(ns)
+	for i := 0; i < ln; i++ {
+		for j := i + 1; j < ln; j++ {
+			for k := 0; k < ln; k++ {
+				if ns[i]+ns[j]+ns[k] == 2020 {
+					return ns[i] * ns[j] * ns[k]
+				}
+			}
+		}
+	}
+
+	return -1 // unreachable
+}
+
+func parseInput(input string) []int {
+	lines := strings.Split(input, "\n")
+	nums := []int{}
+	for _, l := range lines {
+		n, err := strconv.Atoi(l)
+		if err != nil {
+			panic(err)
+		}
+		nums = append(nums, n)
+	}
+	return nums
+}
---
+++ 2020/01/main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/suic86/aoc-go/util"
+	"testing"
+)
+
+var tests1 = []struct {
+	name string
+	want int
+	file string
+}{
+	{"example", 514579, "test.data"},
+	{"input", 1020036, "input.data"},
+}
+
+var tests2 = []struct {
+	name string
+	want int
+	file string
+}{
+	{"example", 241861950, "test.data"},
+	{"input", 286977330, "input.data"},
+}
+
+func TestPart1(t *testing.T) {
+	for _, test := range tests1 {
+		t.Run(test.name, func(*testing.T) {
+			got := part1(util.ReadFile(test.file))
+			if got != test.want {
+				t.Errorf("got: %v, want: %v", got, test.want)
+			}
+		})
+	}
+}
+
+func TestPart2(t *testing.T) {
+	for _, test := range tests2 {
+		t.Run(test.name, func(*testing.T) {
+			got := part2(util.ReadFile(test.file))
+			if got != test.want {
+				t.Errorf("got: %v, want: %v", got, test.want)
+			}
+		})
+	}
+}
