f923560ed3bf6cdbc2b943e5b8a0e733e8a87d0f
suic86 <marko.gabriel.cz@gmail.com>
2024-11-04 22:17:00 +0100

    ReadFileToStringArray function.

--- util/ReadFile.go
+++ util/ReadFile.go
@@ -1,6 +1,8 @@
 package util
 
 import (
+	"bufio"
+	"log"
 	"os"
 	"path"
 	"path/filepath"
@@ -8,12 +10,35 @@ import (
 	"strings"
 )
 
+func ReadFileToStringArray(pathFromCaller string) []string {
+	_, filename, _, ok := runtime.Caller(1)
+	if !ok {
+		panic("Could not find Caller of util.ReadFile")
+	}
+	absPath := path.Join(path.Dir(filename), pathFromCaller)
+	file, err := os.Open(absPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var lines []string
+
+	scanner:= bufio.NewScanner(file)
+	for scanner.Scan(){
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return lines
+}
+
 func ReadFile(pathFromCaller string) string {
 	_, filename, _, ok := runtime.Caller(1)
 	if !ok {
 		panic("Could not find Caller of util.ReadFile")
 	}
-
 	absPath := path.Join(path.Dir(filename), pathFromCaller)
 	content, err := os.ReadFile(absPath)
 	if err != nil {
